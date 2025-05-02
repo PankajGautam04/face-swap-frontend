2c249160c83dc013cb0df2d900227fbc4cc09ce9
Pankaj Gautam <34612449+PankajGautam04@users.noreply.github.com>
2025-05-02 13:18:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -188,7 +188,7 @@ function updateSwapButton() {
     swapButton.classList.toggle('cursor-not-allowed', swapButton.disabled);
     if (loading) swapButton.innerHTML = `<div class="flex items-center"><svg class="animate-spin h-6 w-6 mr-3 text-white" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>Processing...</div>`;
     else if (targetFaces.length > 1 && selectedFaceIndex === null) swapButton.textContent = "Select a Face";
-    else swapButton.textContent = `Swap Faces in ${activeTab === 'image' ? 'Image' : 'Video'}`;
+    else swapButton.textContent = `Swap Face in ${activeTab === 'image' ? 'Image' : 'Video'}`;
 }
 
 async function handleSubmit(e) {
